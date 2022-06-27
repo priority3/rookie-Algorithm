0470cc502329cfdb075b042f706c1cad52ea3407
priority <1832052104@qq.com>
2022-06-27 22:40:59 +0800

    feat: 1160.py

---
+++ easy/1160/1160.py
@@ -0,0 +1,15 @@
+# str = 'abcka'
+# print(str.count('a'))
+import collections
+from typing import Collection
+
+
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        res = 0 
+        cnt = collections.Counter(chars)
+        for w in words:
+            cnt_w = Collection.Counter(w)
+            if all([cnt_w[i]<cnt[i] for i in w]):
+                res += len(w)
+        return res
