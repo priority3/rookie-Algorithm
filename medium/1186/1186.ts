0f8c47d20900870561bffdaf1c11f4b6734bf20c
priority <1832052104@qq.com>
2022-06-29 22:07:19 +0800

    feat: 1186

---
+++ medium/1186/1186.ts
@@ -0,0 +1,13 @@
+function maximumSum(arr: number[]): number {
+  let [a, res, b] = [arr[0], 0, arr[0]]
+  for (let i = 1; i < arr.length; i++) {
+    // 以及删除
+    b = Math.max(b + arr[i], a)
+    // 未删除
+    a = Math.max(a + arr[i], arr[i])
+    res = Math.max(res, a, b)
+  }
+  return res
+}
+console.log(maximumSum([1, -2, 0, 3]))
+
