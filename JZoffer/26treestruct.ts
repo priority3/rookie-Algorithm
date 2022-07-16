b0cb71ab580a97948ce6b3390c9e4fa688295abc
priority <1832052104@qq.com>
2022-07-16 22:28:45 +0800

    feat: jzoffer 7

---
+++ JZoffer/26treestruct.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSubStructure(A: TreeNode | null, B: TreeNode | null): boolean {
+  if (!B || !A)
+    return false
+  const dfs = (A: TreeNode | null, B: TreeNode | null): boolean => {
+    if (!B)
+      return true
+    if (!A)
+      return false
+    return A.val === B.val && dfs(A.left, B.left) && dfs(A.right, B.right)
+  }
+  return dfs(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B)
+}
---
+++ JZoffer/28mirTree.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSymmetric(root: TreeNode | null): boolean {
+  if (!root)
+    return false
+  const dfs = (left: TreeNode | null, right: TreeNode | null): boolean => {
+    if (!left && !right)
+      return true
+    if (!left || !right || left.val !== right.val)
+      return false
+    return left.val === right.val && dfs(left.left, right.right) && dfs(left.right, right.left)
+  }
+  return dfs(root.left, root.right)
+}
