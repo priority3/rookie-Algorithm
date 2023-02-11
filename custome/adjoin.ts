a5294c02e029adda7cc985f4005201af96e07821
priority <1832052104@qq.com>
2023-02-11 18:02:24 +0800

    style: lint

--- custome/adjoin.ts
+++ custome/adjoin.ts
@@ -1,27 +1,27 @@
 // 描述
 /**
- * 实现一个adjoin 
+ * 实现一个adjoin
  * adjoin([1,2,3,4,5],item => item !== 3) ----> [[1,2],3,[4,5]]
  * adjoin([1,2,3,4],item => item < 3) ----> [[1,2],3,4]
- * 
+ *
  */
 type resArr = Array<number | Array<number>>
 
-const adjoin = (arr:Array<number>,fn:Function):resArr => {
-  //number[] not property -> flatMap   ts-target:es2019
-  let res:resArr = []
-  arr.forEach(item => {
-    if(fn(item)){
-      let lastIndex = res.length-1
-      // 是否是数组
-      Object.prototype.toString.call(res[lastIndex]).slice(8,-1) === 'Array' 
-      ? (res[lastIndex] as Array<number>).push(item) : res.push([item])
-    }else{
-      res.push(item)
-    }
-  }) 
-  return res
+const adjoin = (arr: Array<number>, fn: Function): resArr => {
+    //number[] not property -> flatMap   ts-targets:es2019
+    let res: resArr = []
+    arr.forEach(item => {
+        if (fn(item)) {
+            let lastIndex = res.length - 1
+            // 是否是数组
+            Object.prototype.toString.call(res[lastIndex]).slice(8, -1) === 'Array'
+                ? (res[lastIndex] as Array<number>).push(item) : res.push([item])
+        } else {
+            res.push(item)
+        }
+    })
+    return res
 }
 
 
-console.log(adjoin([1,2,3,4,5],(item:number) => item < 3));
+console.log(adjoin([1, 2, 3, 4, 5], (item: number) => item < 3));
--- easy/35/35. Search Insert Position.py
+++ easy/35/35. Search Insert Position.py
@@ -1,23 +1,23 @@
 from typing import List
-import bisect
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-      # return bisect.bisect_right(nums,target)
-      
-      left, right = 0, len(nums)-1
-      if target > nums[right]:
-        return len(nums)
-      while left < right:
-        mid = (left + right) >> 1
-        if nums[mid] == target:
-          return mid
-        elif nums[mid] > target:
-          right = mid
-        else:
-          left = mid + 1
-      return left
-          
 
-            
+
+class Solution:
+  def searchInsert(self, nums: List[int], target: int) -> int:
+    # return bisect.bisect_right(nums,targets)
+    
+    left, right = 0, len(nums) - 1
+    if target > nums[right]:
+      return len(nums)
+    while left < right:
+      mid = (left + right) >> 1
+      if nums[mid] == target:
+        return mid
+      elif nums[mid] > target:
+        right = mid
+      else:
+        left = mid + 1
+    return left
+
+
 p = Solution()
-print(p.searchInsert([1,3,5,6],0))
+print(p.searchInsert([1, 3, 5, 6], 0))
