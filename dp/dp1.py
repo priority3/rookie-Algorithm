59363ff0b49e5f8aa4516c378c99934a1060df5e
priority <1832052104@qq.com>
2023-05-01 22:00:52 +0800

    feat: dp feat 64

--- dp/dp1.py
+++ dp/dp1.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 # frog problem
 def dp_frog(n):
   dp = [0] * (n)
@@ -21,6 +24,24 @@ def uniquePaths(m: int, n: int) -> int:
   return dp[m - 1][n - 1]
 
 
+# leetcode_64
+def minPathSum(grid: List[List[int]]):
+  m, n = len(grid), len(grid[0])
+  dp = [[0] * n for _ in range(m)]
+  # init dp
+  for i in range(m):
+    dp[i][0] = grid[i][0] + dp[i - 1][0]
+  for i in range(n):
+    dp[0][i] = grid[0][i] + dp[0][i - 1]
+  print(dp)
+  for i in range(m):
+    for j in range(n):
+      if i > 0 and j > 0:
+        dp[i][j] = (grid[i][j] + min(dp[i - 1][j], dp[i][j - 1]))
+  return dp[m - 1][n - 1]
+
+
 if __name__ == '__main__':
   # print('frog problem:', dp_frog(10))
-  print('leetcode_62:', uniquePaths(3, 7))
+  # print('leetcode_62:', uniquePaths(3, 7))
+  print('leetcode_64:', minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]]))
--- hard/440. 字典序的第K小数字.py
+++ hard/440. 字典序的第K小数字.py
@@ -1,30 +1,29 @@
-from itertools import count
-
-
 class Solution:
+  
+  def findKthNumber(self, n: int, k: int) -> int:
+    
+    def getCount(prefix: int, n: int) -> int:
+      cur, next, count = prefix, prefix + 1, 0
+      while cur <= n:
+        count += min(n + 1, next) - cur
+        cur *= 10
+        next *= 10
+      return count
+    
+    # 计数 前缀
+    p, prefix = 1, 1
+    while p < k:
+      count = getCount(prefix, n)
+      # 在当前前缀范围内
+      if p + count > k:
+        # 前缀下移
+        prefix *= 10
+        p += 1
+      else:
+        # 不在当前范围内
+        p += count
+        prefix += 1
+    return prefix
 
-    def findKthNumber(self, n: int, k: int) -> int:
 
-        def getCount(prefix: int, n: int) -> int:
-            cur, next, count = prefix, prefix + 1, 0
-            while cur <= n:
-                count += min(n + 1, next) - cur
-                cur *= 10
-                next *= 10
-            return count
-
-        # 计数 前缀
-        p, prefix = 1, 1
-        while p < k:
-            count = getCount(prefix, n)
-            # 在当前前缀范围内
-            if p + count > k:
-              # 前缀下移
-                prefix *= 10
-                p += 1
-            else:
-              # 不在当前范围内
-                p += count
-                prefix += 1
-        return prefix
 print(Solution().findKthNumber(10, 3))
