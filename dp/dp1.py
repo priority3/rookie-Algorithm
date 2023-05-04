728099438ddd887581027a3fcd011fe8db3c8ccd
priority <1832052104@qq.com>
2023-05-04 15:04:34 +0800

    feat: dp feat 72

--- dp/dp1.py
+++ dp/dp1.py
@@ -40,6 +40,24 @@ def minPathSum(grid: List[List[int]]):
   return dp[m - 1][n - 1]
 
 
+# leetcode_72
+def minDistance(word1: str, word2: str) -> int:
+  m, n = len(word1), len(word2)
+  dp = [[0] * (n + 1) for _ in range(m + 1)]
+  for i in range(1, m):
+    dp[i][0] = dp[i - 1][0] + 1
+  for i in range(1, n):
+    dp[0][i] = dp[0][i - 1] + 1
+  
+  for i in range(1, m + 1):
+    for j in range(1, n + 1):
+      if word1[i - 1] == word2[j - 1]:
+        dp[i][j] = dp[i - 1][j - 1]
+      else:
+        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+  return dp[m][n]
+
+
 if __name__ == '__main__':
   # print('frog problem:', dp_frog(10))
   # print('leetcode_62:', uniquePaths(3, 7))
