30cda1b74b75cce57da9efb1debae8e7edb58ba2
priority <1832052104@qq.com>
2023-05-04 15:42:58 +0800

    fix: dp feat 72 optimum

--- dp/dp_optimum.py
+++ dp/dp_optimum.py
@@ -12,5 +12,23 @@ def uniquePaths(m: int, n: int) -> int:
   return dp[n - 1]
 
 
+# leetcode_72
+def minDistance(word1: str, word2: str) -> int:
+  m, n = len(word1), len(word2)
+  dp = [i for i in range(n + 1)]
+  for i in range(1, m + 1):
+    temp = dp[0]
+    # init
+    dp[0] = i
+    for j in range(1, n + 1):
+      pre = temp
+      temp = dp[j]
+      if word1[i - 1] == word2[j - 1]:
+        dp[j] = pre
+      else:
+        dp[j] = min(dp[j], dp[j - 1], pre) + 1
+  return dp[n]
+
+
 if __name__ == "__main__":
   print(uniquePaths(3, 7))
