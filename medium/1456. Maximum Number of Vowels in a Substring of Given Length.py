e657982eaf041d820d64960b18cac43053c3a8ff
priority <1832052104@qq.com>
2023-05-05 16:30:36 +0800

    fix: 1456 typo

--- medium/1456. Maximum Number of Vowels in a Substring of Given Length.py
+++ medium/1456. Maximum Number of Vowels in a Substring of Given Length.py
@@ -2,22 +2,22 @@ class Solution:
   def maxVowels(self, s: str, k: int) -> int:
     vowels = ['a', 'e', 'i', 'o', 'u']
     m = len(s)
+    # move window
     # dp[i] meaning that the max vowels in s[i-k:i]
-    dp = [0] * m
+    windows = [0] * m
     for i in range(k):
       if s[i] in vowels:
-        dp[i] = dp[i - 1] + 1
+        windows[i] = windows[i - 1] + 1
       else:
-        dp[i] = dp[i - 1]
-    print(dp)
+        windows[i] = windows[i - 1]
     for i in range(k, m):
       if s[i] in vowels and s[i - k] not in vowels:
-        dp[i] = dp[i - 1] + 1
+        windows[i] = windows[i - 1] + 1
       elif s[i] not in vowels and s[i - k] in vowels:
-        dp[i] = dp[i - 1] - 1
+        windows[i] = windows[i - 1] - 1
       else:
-        dp[i] = dp[i - 1]
-    return max(dp)
+        windows[i] = windows[i - 1]
+    return max(windows)
 
 
 p = Solution()
