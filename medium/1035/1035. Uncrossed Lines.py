1488fdc0331d57f38542b60d9840bb3d60a9bf1e
priority <1832052104@qq.com>
2023-05-11 23:37:13 +0800

    fix: 1035

---
+++ medium/1035/1035. Uncrossed Lines.py
@@ -0,0 +1,17 @@
+from functools import cache
+
+
+class Solution:
+  def maxUncrossedLines(self, a: List[int], b: List[int]) -> int:
+    @cache
+    def dp(i: int, j: int) -> int:
+      if i < 0 or j < 0:
+        return 0
+      
+      return max(
+        dp(i - 1, j),
+        dp(i, j - 1),
+        dp(i - 1, j - 1) + int(a[i] == b[j]),
+      )
+    
+    return dp(len(a) - 1, len(b) - 1)
